3c3b86ceda06922d395f8c5266b8c335f914aef1
zby <ytojy2018>
2022-02-26 18:20:15 +0800

    初始化框架(未完成)

--- parse-swagger/src/test/java/com/auto/base/swagger/ParseSwaggerApplicationTests.java
+++ parse-swagger/src/test/java/com/auto/base/swagger/ParseSwaggerApplicationTests.java
@@ -1,11 +1,20 @@
 package com.auto.base.swagger;
 
+import com.alibaba.fastjson.JSONObject;
+import com.auto.base.swagger.domain.ApiDefinitionWithBLOBs;
+import com.auto.base.swagger.mock.MockApiUtils;
 import com.auto.base.swagger.parse.ApiDefinitionImport;
 import com.auto.base.swagger.parse.ApiTestImportRequest;
 import com.auto.base.swagger.parse.Swagger2Parser;
+import com.auto.base.swagger.request.MsHTTPSamplerProxy;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 //@SpringBootTest
+@Slf4j
 class ParseSwaggerApplicationTests {
 
     @Test
@@ -16,11 +25,23 @@ class ParseSwaggerApplicationTests {
         apiTestImportRequest.setType("schedule");
         apiTestImportRequest.setUserId("user1");
 
-        apiTestImportRequest.setSwaggerUrl("http://camel.dev.yoohoor.com:82/swagger/api.json");
+        ObjectMapper mapper = new ObjectMapper();
+        MsHTTPSamplerProxy proxy;
+
+        Map<String, String> returnMap;
+
+
+        apiTestImportRequest.setSwaggerUrl("http://camel.dev.yoohoor.com:9003/cms/carrier/v2/api-docs?group=cms-carrier");
+//        apiTestImportRequest.setSwaggerUrl("http://camel.dev.yoohoor.com:82/swagger/api.json");
         ApiDefinitionImport result = new Swagger2Parser().parse(null, apiTestImportRequest);
+        for (ApiDefinitionWithBLOBs item : result.getData()) {
+            //解析方式有问题
+//                proxy = mapper.readValue(item.getRequest(), new TypeReference<MsHTTPSamplerProxy>() {});
+            proxy = JSONObject.parseObject(item.getRequest(), MsHTTPSamplerProxy.class);
+            returnMap = MockApiUtils.getApiResponse(item.getResponse());
 
 
-
+            System.out.println("end");
+        }
     }
-
 }
